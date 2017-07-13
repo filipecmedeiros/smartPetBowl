941be26829a3af08be2d749ab91553f342316e3b
Filipe Medeiros <fcma@cin.ufpe.br>
2017-07-12 21:04:21 -0300

    Resolvendo bug do modal

--- core/static/modal2.js
+++ core/static/modal2.js
@@ -1,29 +1,25 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+var modall = document.getElementById('myModall');
 
 // Get the button that opens the modal
-var btn = document.getElementById("modal");
-
-
-var btn2 = document.getElementById("myBtn");
-
+var btnn = document.getElementById("modall");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
-    modal.style.display = "block";
+    modall.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
+    modall.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modall) {
+        modall.style.display = "none";
     }
 }
